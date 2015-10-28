a1908b0f1e88e531adca083fbea26e72fe83983a
alu0100763379 <alu0100@ull.edu.es>
2015-10-28 18:52:15 +0000

    Realizada y Comprobada con tests la multiplicación

--- lib/racional.rb
+++ lib/racional.rb
@@ -1,4 +1,19 @@
-require "example/gcd.rb"
+#require "example/gcd.rb"
 
 class Fraccion
+    attr_reader :num, :den
+    
+    def initialize(num, den)
+        @num = num
+        @den = den
+       # gcd(@num,@den) #Simplificamos la fracción
+    end
+    
+    def to_s
+        "#{@num}/#{@den}"
+    end
+    
+    def *(object)
+        Fraccion.new(@num*object.num, @den*object.den)
+    end
 end
